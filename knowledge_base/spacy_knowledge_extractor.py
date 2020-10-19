f5770acf1ea5f557e188e22d6dba940884578740
Ubaer <jetten.kevin@gmail.com>
2020-10-19 21:12:15 +0200

    Custom names can be added to make experiments more reliable when participants have uncommon names that are not known by the spaCy library.

--- knowledge_base/spacy_knowledge_extractor.py
+++ knowledge_base/spacy_knowledge_extractor.py
@@ -3,6 +3,9 @@ import spacy
 # if "Can't find model 'en_core_web_sm'" download it by using: python -m spacy download en_core_web_sm
 nlp = spacy.load("en_core_web_sm")
 
+# Some names aren't recognized by spaCy. To ensure reliability of an experiment put the names of participants here
+custom_names = {'PseudoName1', 'PseudoName2'}
+
 
 # Named Entity Recognition using spaCy
 def extract_person(text):
@@ -14,8 +17,13 @@ def extract_person(text):
     person = None
     for ent in doc.ents:
         if ent.label_ == "PERSON":
-            # print(ent.text, ent.start_char, ent.end_char, ent.label_)
             person = ent.text
+
+    if person is None:
+        for custom_name in custom_names:
+            if custom_name.lower() in text.lower():
+                person = custom_name
+                break
     return person
 
 
